22894c81c5d9f9d7466da433e9e74068c5c6acc4
AbdoJohanen <abdojoh@hotmail.com>
2023-10-18 11:25:01 +0200

    Created ProductEntity

---
+++ Manero/Models/Entities/ProductEntities/ProductEntity.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Manero.Models.Entities.ProductEntities;
+
+public class ProductEntity
+{
+    [Key]
+    public string ArticleNumber { get; set; } = null!;
+    public string ProductName { get; set; } = null!;
+    public string? ProductDescription { get; set; }
+    public decimal ProductPrice { get; set; }
+    public decimal? ProductDiscount { get; set; }
+
+    // ICollections go under here
+
+}
