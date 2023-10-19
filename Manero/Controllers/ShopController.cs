adb5a94d7df04c99cbed3e01c4ac2802076456d3
Elvin Javadov <Javadov@hotmail.com>
2023-10-19 14:33:05 +0200

    Navbar och Controller skapades

---
+++ Manero/Controllers/ShopController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Manero.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.ActivePage = "Shop";
+            return View();
+        }
+    }
+}
