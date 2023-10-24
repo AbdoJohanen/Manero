ab09b796dc4a2254d8de3a7c87fd9b5dfa7a54e9
Lucas Nykvist <lucasnykvist01@gmail.com>
2023-10-24 23:10:51 +0200

    added controller and view for shop page

---
+++ Manero/Controllers/ShopController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Manero.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
