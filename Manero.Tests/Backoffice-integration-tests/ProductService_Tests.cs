64b15d14e407ed69a90c1e9b51c811e83474799b
Ludvig Franck <ludvig.franck@gmail.com>
2023-10-26 14:33:35 +0200

    Testing for backoffice added

---
+++ Manero.Tests/Backoffice-integration-tests/ProductService_Tests.cs
@@ -0,0 +1,39 @@
+﻿using Manero.Contexts;
+using Manero.Helpers.Repositories.DataRepositories;
+using Manero.Helpers.Services.DataServices;
+using Manero.Models.DTO;
+using Microsoft.EntityFrameworkCore;
+
+namespace Manero.Tests.Backoffice_integration_tests;
+
+public class ProductService_Tests
+{
+    private readonly ProductService _service;
+    private readonly ProductRepository _repository;
+    private readonly DataContext _context;
+
+    public ProductService_Tests()
+    {
+        var options = new DbContextOptionsBuilder<DataContext>()
+        .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+        .Options;
+
+        _context = new DataContext(options);
+        _repository = new ProductRepository(_context);
+        _service = new ProductService(_repository);
+    }
+
+    [Fact]
+    public async Task CreateProductAsync_Should_ReturnProductModel_When_CreatedSuccessfully()
+    {
+        // Arrange
+        var product = new ProductModel() { ArticleNumber = "Test-1", ProductName = "Test", ProductPrice = 0 };
+
+        // Act
+        var result = await _service.CreateProductAsync(product);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsType<ProductModel>(result);
+    }
+}
---
+++ Manero.Tests/Backoffice-integration-tests/ProductTagService_Tests.cs
@@ -0,0 +1,44 @@
+﻿using Manero.Contexts;
+using Manero.Helpers.Repositories.DataRepositories;
+using Manero.Helpers.Services.DataServices;
+using Manero.Models.DTO;
+using Microsoft.EntityFrameworkCore;
+
+namespace Manero.Tests.Backoffice_integration_tests;
+
+public class ProductTagService_Tests
+{
+    private readonly ProductTagService _service;
+    private readonly ProductTagRepository _repository;
+    private readonly DataContext _context;
+
+    public ProductTagService_Tests()
+    {
+        var options = new DbContextOptionsBuilder<DataContext>()
+        .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+        .Options;
+
+        _context = new DataContext(options);
+        _repository = new ProductTagRepository(_context);
+        _service = new ProductTagService(_repository);
+    }
+
+    [Fact]
+    public async Task AssociateTagsWithProductAsync_Should_AssociateOneProductModelWithListOfTagModel()
+    {
+        //Arrange
+        var tags = new List<TagModel>() { new TagModel { Id = 1, Tag = "Test" }, new TagModel { Id = 2, Tag = "Test" } };
+        var product = new ProductModel() { ArticleNumber = "Test-1", ProductName = "Test", ProductPrice = 0 };
+
+        //Act
+        await _service.AssociateTagsWithProductAsync(tags, product);
+        var productTags = await _repository.GetAllAsync(x => x.ArticleNumber == product.ArticleNumber);
+
+        //Assert
+        Assert.NotNull(productTags);
+        foreach (var item in productTags)
+            Assert.Equal(item.ArticleNumber, product.ArticleNumber);
+
+        Assert.Equal(tags.Count(), productTags.Count());
+    }
+}
