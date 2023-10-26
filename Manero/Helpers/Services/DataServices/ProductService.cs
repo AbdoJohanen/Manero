8affd96818b882c654afd762ada8fccecd979456
Ludvig Franck <ludvig.franck@gmail.com>
2023-10-26 10:05:03 +0200

    Clean up in code

--- Manero/Helpers/Services/DataServices/ProductService.cs
+++ Manero/Helpers/Services/DataServices/ProductService.cs
@@ -64,8 +64,7 @@ public class ProductService
     }
 
 
-    // Calculate product discount price
-    /*
+    //Calculate product discount price
     private decimal? CalcProductPrice(ProductModel model)
     {
         if (model != null && model.ProductDiscount != null)
@@ -73,67 +72,7 @@ public class ProductService
             var discountPrice = model.ProductPrice * (model.ProductDiscount / 100);
             return discountPrice;
         }
-        
+
         return model!.ProductPrice;
     }
-    */
 }
-
-
-
-
-// OLD REFRENCE
-/*
-public class ProductService
-{
-
-
-private readonly CategoryService _categoryService;
-private readonly ProductRepository _productRepo;
-private readonly ProductCategoryRepository _productCategoryRepo;
-private readonly ImageService _imageService;
-
-public ProductService(CategoryService categoryService, ImageService imageService, ProductCategoryRepository productCategoryRepo, ProductRepository productRepo)
-{
-    _categoryService = categoryService;
-    _imageService = imageService;
-    _productCategoryRepo = productCategoryRepo;
-    _productRepo = productRepo;
-}
-
-public async Task<bool> SaveProductAsync(CreateProductViewModel model)
-{
-    if (model != null)
-    {
-
-        var _findProduct = await _productRepo.GetAsync(x => x.Name == model.Name);
-        if (_findProduct == null)
-        {
-            // If product dosent exists, make a new
-            ProductEntity _product = model;
-            _product = await _productRepo.AddAsync(_product);
-
-            // Handel Images
-            if (model.Image != null)
-            {
-                await _imageService.SaveProductImageAsync(_product, model.Image);
-            }
-
-            // Handle Categories
-
-            foreach (var category in model.Categories)
-            {
-                if (category.isActive == true)
-                {
-                    await _productCategoryRepo.AddAsync(new ProductCategoryEntity { productId = _product.Id, categoryId = category.Id });
-                }
-            }
-
-            return true;
-        }
-    }
-    return false;
-}
-}
-
-*/
