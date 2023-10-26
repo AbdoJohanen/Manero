a3cf8437b4ec70d4e9fc81d274fe8d8f8919b9ab
SimonBacklin <SimonBacklin@hotmail.com>
2023-10-26 10:32:48 +0200

    Fix merge error in script.js

--- Manero/wwwroot/js/script.js
+++ Manero/wwwroot/js/script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log('DOM fully loaded and parsed');
 
     const inputElement = document.querySelector('input[name="Images"]');
-    console.log('Input Element:', inputElement); 
+    console.log('Input Element:', inputElement);
 
     if (inputElement) {
         inputElement.addEventListener('change', function (event) {
@@ -14,23 +14,23 @@ document.addEventListener("DOMContentLoaded", function () {
             previewContainer.innerHTML = '';
 
             const files = event.target.files;
-            console.log('Files:', files); 
+            console.log('Files:', files);
 
             for (let i = 0; i < files.length; i++) {
                 const file = files[i];
 
                 const reader = new FileReader();
                 reader.addEventListener('load', function (loadEvent) {
-                    
+
                     const imageDiv = document.createElement('div');
-                    imageDiv.classList.add('m-3'); 
-                    
+                    imageDiv.classList.add('m-3');
+
                     const imgElement = document.createElement('img');
                     imgElement.src = loadEvent.target.result;
                     imgElement.alt = file.name;
-                    imgElement.style.width = '100px'; 
+                    imgElement.style.width = '100px';
                     imgElement.classList.add('me-3');
-                    
+
                     const radioButton = document.createElement('input');
                     radioButton.type = 'radio';
                     radioButton.name = 'MainImageFileName';
@@ -38,12 +38,12 @@ document.addEventListener("DOMContentLoaded", function () {
                     if (i === 0) {
                         radioButton.checked = true;
                     }
-                    
+
                     const label = document.createElement('label');
                     label.innerText = ` Set as Main Product Image`;
                     label.classList.add('ms-1');
 
-                    
+
                     imageDiv.appendChild(imgElement);
                     imageDiv.appendChild(radioButton);
                     imageDiv.appendChild(label);
@@ -55,7 +55,8 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     } else {
         console.error('Input element not found. Check if the selector is correct.');
-
+    }
+});
 window.addEventListener('load', function () {
     const hasVisitedBefore = localStorage.getItem('hasVisitedBefore');
 
