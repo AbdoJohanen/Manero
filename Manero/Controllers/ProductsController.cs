5cb2ba3e0a0adad32eee232ee32b73a90717f623
Mira Wänseth <mira.wanseth@utb.ecutbildning.se>
2023-10-27 10:34:24 +0200

    Added ProductsController

---
+++ Manero/Controllers/ProductsController.cs
@@ -0,0 +1,15 @@
+﻿using Manero.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Manero.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
