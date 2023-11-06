5353c6a98a296783a2dc2f7faed4f7d42fb66d50
AbdoJohanen <abdojoh@hotmail.com>
2023-11-06 19:24:23 +0100

    Created AddressViewModel to handle AddAddress, Address and EditAddress.cshtml

--- Manero/ViewModels/User/AddressViewModel.cs
+++ Manero/ViewModels/User/AddressViewModel.cs
@@ -1,9 +1,33 @@
-﻿namespace Manero.ViewModels.User;
+﻿using Manero.Models.Entities.UserEntities;
+using System.ComponentModel.DataAnnotations;
+
+namespace Manero.ViewModels.User;
 
 public class AddressViewModel
 {
-    public int Id { get; set; }
+    public string? UserId { get; set; }
+
+    [Display(Name = "Title")]
+    public string AddressTitle { get; set; } = null!;
+
+    [Display(Name = "Street Name")]
     public string StreetName { get; set; } = null!;
+
+    [Display(Name = "Postal Code")]
     public string PostalCode { get; set; } = null!;
+
+    [Display(Name = "City")]
     public string City { get; set; } = null!;
+
+    public static implicit operator AddressEntity(AddressViewModel model)
+    {
+        return new AddressEntity
+        {
+            StreetName = model.StreetName,
+            PostalCode = model.PostalCode,
+            City = model.City
+        };
+    }
+
+    public ICollection<UserAddressEntity> Addresses { get; set; } = new HashSet<UserAddressEntity>();
 }
