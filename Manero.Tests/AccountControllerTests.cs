6268f70c80c36a373ce91b838996deb75e0099cd
AbdoJohanen <abdojoh@hotmail.com>
2023-11-13 11:38:07 +0100

    Started creating test

--- Manero.Tests/AccountControllerTests.cs
+++ Manero.Tests/AccountControllerTests.cs
@@ -2,6 +2,7 @@
 using Manero.Controllers;
 using Manero.Helpers.Services.UserServices;
 using Manero.Models.Entities.UserEntities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +13,23 @@ namespace Manero.Tests;
 
 public class AccountControllerTests
 {
+    private readonly AddressService _addressService;
     private readonly AccountController _accountController;
+    private readonly IdentityContext _identityContext;
+
+    public AccountControllerTests(AddressService addressService, AccountController accountController, IdentityContext identityContext)
+    {
+        var options = new DbContextOptionsBuilder<IdentityContext>()
+                        .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                        .Options;
+
+        _addressService = addressService;
+        _accountController = accountController;
+        _identityContext = identityContext;
+    }
 
     [Fact]
-    public async Task AddAddress_Should_AddAddressEntityAndUserAddressentity_To_AzureSqlDatabase()
+    public async Task AddAddress_Should_AddAddressEntityAndUserAddressEntity_To_SqlDatabase()
     {
         // Arrange
         AddressEntity addressEntity = new()
@@ -26,9 +40,18 @@ public class AccountControllerTests
             City = "Örebro"
         };
 
+
+
         // Act
+        var result = await _addressService.GetorCreateAsync(addressEntity);
 
         // Assert
+        Assert.NotNull(result);
+        Assert.IsType<AddressEntity>(result);
+        Assert.Equal(addressEntity.StreetName, result.StreetName);
+
+
+
 
     }
 }
